081f84ffef1be2b08d547adae765e9829862e8cb
100007217 <gomezmonterroso14@gmail.com>
2021-11-19 01:45:51 +0100

    Actividad 8

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,19 @@ function validar() {
         return true;
     } else if (email == "" && password == "") {
         document.getElementById("mensaje").innerHTML = "Falta mail y password";
+        document.getElementById("email").style.borderColor = "red"
+        document.getElementById("password").style.borderColor = "red"
         return false
     } else if (email == "" || password == "") {
         if (email == "") {
             document.getElementById("mensaje").innerHTML = "Falta mail";
+            document.getElementById("email").style.borderColor = "red"
+            document.getElementById("password").style.borderColor = "#4CAF50"
             return false
         } else {
             document.getElementById("mensaje").innerHTML = "Falta pass";
+            document.getElementById("password").style.borderColor = "red"
+            document.getElementById("email").style.borderColor = "#4CAF50"
             return false
         }
     }
